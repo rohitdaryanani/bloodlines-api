0a70b0c4c4eb085dad99e88fc9cce27bfa558b17
Rohit Daryanani <rohitdaryanani@yahoo.com>
2015-04-17 11:19:27 +0800

    refactor test for get person by id

--- test/persons.js
+++ test/persons.js
@@ -12,38 +12,36 @@ var expect   = Code.expect;
 var server = require( '../server' );
 
 var peoplejson = require('fs').readFileSync(path.join(__dirname, '../people.json'));
-    peoplejson = peoplejson.toString();
-    peoplejson = peoplejson.replace(/\n/g, ',').replace(/.$/g, '');
-    peoplejson = '[' + peoplejson.split('\n').join(',') + ']';
+	peoplejson = peoplejson.toString();
+	peoplejson = peoplejson.replace(/\n/g, ',').replace(/.$/g, '');
+	peoplejson = '[' + peoplejson.split('\n').join(',') + ']';
 
 var people = JSON.parse(peoplejson);
 
 describe( 'Persons', function () {
 
-    describe( 'GET', function () {
-        it( 'should GET a specific person by id', function ( done ) {
-            var options = {
-                method : 'GET',
-                url    : '/person/' + people[0]._id
-            };
+	describe( 'GET', function () {
+		it( 'should GET a specific person by id', function ( done ) {
+			var options = {
+				method : 'GET',
+				url    : '/person/' + people[0]._id['$oid']
+			};
 
-            server.inject( options, function ( response ) {
-                var result = response.result;
+			server.inject( options, function ( response ) {
+				var result = response.result;
 
-                expect( response.statusCode ).to.equal( 200 );
-                expect( result ).to.be.an.object();
-                expect( result.firstName ).to.equal( people['firstName'] );
-                expect( result.lastName ).to.equal( people['lastName'] );
-                expect( result.contactNumber ).to.equal( people['contactNumber'] );
-                expect( result.bloodType ).to.equal( people['bloodType'] );
-                expect( result.status ).to.equal( people['status'] );
-                console.log( result );
+				expect( response.statusCode ).to.equal( 200 );
+				expect( result ).to.be.an.object();
+				expect( result.firstName ).to.equal( people[0].firstName );
+				expect( result.lastName ).to.equal( people[0].lastName );
+				expect( result.contactNumber ).to.equal( people[0].contactNumber );
+				expect( result.bloodType ).to.equal( people[0].bloodType );
+				expect( result.status ).to.equal( people[0].status );
 
+				done();
+			} )
+		} )
 
-                done();
-            } )
-        } )
-
-    } );
+	} );
 
 } );
