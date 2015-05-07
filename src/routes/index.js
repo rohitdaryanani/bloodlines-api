fbb760f0b31c687a6a2a427878f79d98c8744cff
Rohit Daryanani <rohitdaryanani@yahoo.com>
2015-05-07 17:14:53 +0800

    generate jwt token

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,28 +1,42 @@
-var Person = require( 'mongoose' ).model('Person');
+var Person      = require( 'mongoose' ).model('Person');
+var jwt         = require( 'jsonwebtoken' );
+var superSecret = 'awesome';
+
 
 module.exports = [
 	{
 		method : 'POST',
 		path   : '/login',
-		handler : function ( req, rep ) {
-			req.payload = req.payload || {};
-			var email = req.payload.email;
-			var password = req.payload.password;
-			
+		handler : function ( request, reply ) {
+			request.payload = request.payload || {};
+			var email       = request.payload.email;
+			var password    = request.payload.password;
+
 			var errorMessage = {
 				error : 'invalid email or password'
 			};
-			
+
 			if ( !email || !password ) {
-				return rep( errorMessage ); 
+				return reply( errorMessage );
 			}
-			
+
 			Person.login( email, password, function (err, person) {
 				if( err ) {
-					return rep( errorMessage )
+					return reply( errorMessage )
 				}
-				
-				rep( person );
+
+				// generate token
+				var token = jwt.sign( {
+					email : email,
+				}, superSecret, {
+					expiresInMinutes : 1440 // expires in 24 hours
+				} );
+
+				reply( {
+					person  : person,
+					success : true,
+					token   : token
+				} );
 			});
 		}
 	}
