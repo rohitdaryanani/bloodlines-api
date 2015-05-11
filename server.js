401abf83b743aa5740315dae673c5239a16c1b1e
Rohit Daryanani <rohitdaryanani@yahoo.com>
2015-05-11 19:36:59 +0800

    add mongolab url

--- server.js
+++ server.js
@@ -5,11 +5,12 @@ var Good     = require( 'good' );
 var _        = require( 'lodash' );
 var mongoose = require( 'mongoose' );
 
-var SALT = process.env.APP_SALT || '$2a$10$RSh34k8JX7./qG3ODWyae.';
-var PORT = process.env.PORT || 8000;
+var SALT      = process.env.APP_SALT || '$2a$10$RSh34k8JX7./qG3ODWyae.';
+var PORT      = process.env.PORT || 8000;
+var MONGO_URL = process.env.MONGO_URL || 'mongodb://localhost/test'
 
 // connect to mongo
-mongoose.connect( 'mongodb://localhost/test' );
+mongoose.connect( MONGO_URL );
 require('./src/models/person');
 
 // routes
